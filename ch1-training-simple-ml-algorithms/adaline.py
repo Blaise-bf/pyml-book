dc190d388d0abb2294f51e628ff8524ed11edd80
Blaise Forgwa <barcheblaise@gmail.com>
2026-02-25 23:52:33 +0100

    feat: added adaline sdg logic

--- ch1-training-simple-ml-algorithms/adaline.py
+++ ch1-training-simple-ml-algorithms/adaline.py
@@ -15,7 +15,7 @@ class AdalineGD:
         Random number generator seed for random weight initialization.
     Attributes
     ---------
-    w: 1d-aray
+    w_: 1d-aray
         Weigts after training
     b_: Scalare
         Bias unit after training
@@ -72,3 +72,113 @@ class AdalineGD:
         '''Return class label after unit step'''
         return np.where(self.activation(self.net_input(X))\
         >= .5, 1, 0)
+
+class AdalineSGD:
+
+    '''ADAptive Linear Neuropn Classifier.
+    
+    Parameters
+    ---------
+    eta: float 
+        Learning rate (between 0.0 and 1.0)
+
+    n_iter: int 
+        Passes over the training dataset.
+    random_state: int
+        Random number generator seed for random weight initialization.
+    Attributes
+    ---------
+    w_: 1d-aray
+        Weigts after training
+    b_: Scalare
+        Bias unit after training
+    losses_: list
+        Mean squared error loss function val;ues in each epoch
+    '''
+    def __init__(self, eta=0.2, n_iter=50,random_state=1,
+                shuffle=True, w_initialize=False ):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.shuffle = shuffle
+        self.random_state = random_state
+        self.w_initialized = w_initialize
+
+    def fit(self, X, y):
+        '''Fit training data
+
+        Parameters
+        ---------
+        X: {array-like}, shape: [n_samples, n_features]
+            Training vectors where n_samples is the number of 
+            examples and n_featues  is the number  of features.
+        y: array-like: shape = [n_samples]
+
+        Returns
+        --------
+        self: object
+        '''
+
+        self._initialize_weights(X.shape[1])
+        self.losses_ = []
+
+        for _ in range(self.n_iter):
+
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
+
+        return self
+
+
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+
+        return self
+
+    
+    def _shuffle(self, X, y):
+        '''Shuffle training data'''
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        '''Initialize weights to small random numbers'''
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
+        self.b_ = np.float64(0.)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        '''Applying Adaline learning rule to update weights'''
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_ += self.eta * 2.0 * xi * (error)
+        self.b_ += self.eta * 2.0 * error
+        loss = error**2
+        return loss
+    def net_input(self, X):
+        '''Calculate net input'''
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        '''Compute linear activation'''
+        return X 
+    def predict(self, X):
+        '''Return class label after unit step'''
+        return np.where(self.activation(self.net_input(X))\
+        >= .5, 1, 0)
+
+    
+
+    
